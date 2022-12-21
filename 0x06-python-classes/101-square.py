95519fb2a1cac9d21f3e4735b994ffb5dbccf1be
Issahgordon <issahgordon3@gordon3@mail.com>
2022-12-21 07:35:23 -0800

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 101-square.py
+
 """Define a class Square."""
 
 
@@ -15,7 +15,6 @@ class Square:
         """
         self.size = size
         self.position = position
-
 
     @property
     def size(self):
