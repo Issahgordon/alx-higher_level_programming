ea80dc26a4916f6d42c2dbf43ad3683dca3f675b
Issahgordon <issahgordon3@gordon3@mail.com>
2023-01-05 01:12:54 -0800

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 0-rectangle.py
-""" File name : 0-rectangle.py
-    Simple rectangle : an empty class Rectangle that defines a rectangle
-    It is not allowed to import any module
-"""
+"""Defines a Rectangle class."""
 
 
-class Rectangle(object):
-    """Rectangle: Define new class"""
+class Rectangle:
+    """Represent a rectangle."""
     pass
